6eaecbd000dab617ab705f547a29e95d459aa3ee
Brian Duffy <bgd1112@gmail.com>
2021-07-04 17:53:41 -0400

    fixed site url

--- src/About.tsx
+++ src/About.tsx
@@ -10,7 +10,7 @@ export default function About(): Preact.JSX.Element {
         engineering disasters their causes. Its a running joke to measure the compensation for victims and their
         families as the equivalent number of Xboxs. Thus the calculator was born.</p>
       <p className={styles.italicA}>This site was made over the long fourth of July weekend of 2021
-        by <a href="brian-duffy.dev">Brian Duffy</a>.</p>
+        by <a href="https://brian-duffy.dev">Brian Duffy</a>.</p>
       <p className={styles.taCenter}><a href="/">Go back to the calculator</a></p>
     </section>
   );
