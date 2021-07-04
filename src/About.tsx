ec6d28c2c0bffd430b0a2ca0f59e6314fbd1f093
Brian Duffy <bgd1112@gmail.com>
2021-07-04 18:57:17 -0400

    add linkback

--- src/About.tsx
+++ src/About.tsx
@@ -10,7 +10,8 @@ export default function About(): Preact.JSX.Element {
         engineering disasters their causes. Its a running joke to measure the compensation for victims and their
         families as the equivalent number of Xboxs. Thus the calculator was born.</p>
       <p className={styles.italicA}>This site was made over the long fourth of July weekend of 2021
-        by <a href="https://brian-duffy.dev">Brian Duffy</a>.</p>
+        by <a href="https://brian-duffy.dev">Brian Duffy</a>. This project can also be found
+        on <a href="https://github.com/19dufbri/HowTragic.net">GitHub</a>.</p>
       <p className={styles.taCenter}><a href="/">Go back to the calculator</a></p>
     </section>
   );
